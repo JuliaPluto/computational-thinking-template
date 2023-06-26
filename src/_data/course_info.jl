e75f270fba812ff4cdbaae48792fce60d553087c
lucaferranti <lucaa.ferranti@gmail.com>
2023-06-26 13:16:40 +0300

    more logical handling of authors

--- src/_data/course_info.jl
+++ src/_data/course_info.jl
@@ -4,8 +4,8 @@ Dict(
     "code" => "code of your course",
     "semester" => "Fall 20XX",
     "authors" => [
-        Dict("name" => "First author name", "homepage" => "first author homepage link"),
-        Dict("name" => "Second author name", "homepage" => "second author homepage link")
+        "First author name" => "first author homepage link",
+        "Second author name" => "second author homepage link"
     ],
     "institution" => "Your university (or similar) name",
     "institution_logo" => "name of your institution logo file, e.g. `logo.svg`. Place this under `assets`",
